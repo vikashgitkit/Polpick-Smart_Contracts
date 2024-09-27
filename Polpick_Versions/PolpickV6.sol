ed4f01fb48daf23cbadb1095e42df55f763a956e
Webskitters <vikashofficial9@gmail.com>
2024-09-27 17:34:12 +0530

    Polpick smart contracts versions

---
+++ Polpick_Versions/PolpickV6.sol
@@ -0,0 +1,456 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "hardhat/console.sol";
+
+contract UpVsDownGameV5 is Ownable, ReentrancyGuard {
+
+  struct BetGroup {
+    uint256[] bets;
+    address[] addresses;
+    string[] avatars;
+    string[] countries;
+    string[] whiteLabelIds;
+    uint256 total;
+    uint256 distributedCount;
+    uint256 totalDistributed;
+  }
+
+  struct Round {
+    bool created;
+    int32 startPrice;
+    int32 endPrice;
+    uint256 minBetAmount;
+    uint256 maxBetAmount;
+    uint256 poolBetsLimit;
+    BetGroup upBetGroup;
+    BetGroup downBetGroup;
+    int64 roundStartTime;
+    uint256 tradesStartTimeMS;
+    uint256 tradesEndTimeMS;
+  }
+
+  struct Distribution {
+    uint256 fee;
+    uint256 feeJackpot;
+    uint256 totalMinusFee;
+    uint256 totalMinusJackpotFee;
+    uint256 totalFees;
+    uint256 pending;
+  }
+
+  address public gameController;
+  mapping(bytes => Round) public pools;
+  uint8 public feePercentage = 9;
+  uint8 public feeJackpotPercentage = 1;
+  address public feeAddress = msg.sender; //default fee address
+  address public feeJackpotAddress = msg.sender; //default fee jackpot address
+  bool public isRunning;
+  bytes public notRunningReason;
+  mapping(address => bool) private allowedContracts; //allowed contracts to place trades
+  address[] private allowedContractsList;
+
+  // Errors
+  error PendingDistributions();
+
+  // Events
+  event RoundStarted(bytes poolId, int64 timestamp, int32 price, uint256 minTradeAmount, uint256 maxTradeAmount, uint256 poolTradesLimit, bytes indexed indexedPoolId);
+  event RoundEnded(bytes poolId, int64 timestamp, int32 startPrice, int32 endPrice, bytes indexed indexedPoolId);
+  event TradePlaced(bytes poolId, address sender, uint256 amount, string prediction, uint256 newTotal, bytes indexed indexedPoolId, address indexed indexedSender, string avatarUrl, string countryCode, int64 roundStartTime, string whiteLabelId);
+  event TradeReturned(bytes poolId, address sender, uint256 amount, string whiteLabelId);
+  event GameStopped(bytes reason);
+  event GameStarted();
+  event RoundDistributed(bytes poolId, uint totalWinners, uint from, uint to, int64 timestamp);
+  event TradeWinningsSent(bytes poolId, address sender, uint256 tradeAmount, uint256 winningsAmount, address indexed indexedSender, string whiteLabelId, uint8 feePercentage, uint8 feeJackpotPercentage);
+
+  // Modifiers
+ modifier onlyOpenPool (bytes calldata poolId) {
+    require(isPoolOpen(poolId), 'This pool has a round in progress');
+    _;
+  }
+  constructor(address newGameController) Ownable(newGameController) {
+    gameController = newGameController;
+  }
+
+  function startGame() public onlyOwner {
+    isRunning = true;
+    notRunningReason = '';
+    emit GameStarted();
+  }
+
+  function createPool(bytes calldata poolId, uint256 minBetAmount , uint256 maxBetAmount, uint256 poolBetsLimit) public onlyGameController {
+    pools[poolId].created = true;
+    pools[poolId].minBetAmount = minBetAmount;
+    pools[poolId].maxBetAmount = maxBetAmount;
+    pools[poolId].poolBetsLimit = poolBetsLimit;
+  }
+
+  function isPoolOpen(
+    bytes calldata poolId
+  ) public view returns (bool) {
+    bool a = pools[poolId].startPrice == 0;
+    console.log("IsPool open L94:", a);
+    return a;
+  }
+
+  function trigger(
+    bytes calldata poolId,
+    int64 timeMS,
+    uint256 tradesStartTimeMS,
+    uint256 tradesEndTimeMS,
+    int32 price,
+    uint32 batchSize
+  ) public onlyGameController onlyPoolExists(poolId) {
+    Round storage currentRound = pools[poolId];
+
+    if(isPoolOpen(poolId)) { 
+      console.log("Inside isPoolOpen trigger L109...");
+      require(isRunning, 'The game is not running, rounds can only be ended at this point');
+      currentRound.startPrice = price;
+    
+      currentRound.roundStartTime = timeMS;
+      currentRound.tradesStartTimeMS = tradesStartTimeMS;
+      currentRound.tradesEndTimeMS = tradesEndTimeMS;
+      
+      emit RoundStarted(poolId, timeMS, currentRound.startPrice, currentRound.minBetAmount, currentRound.maxBetAmount, currentRound.poolBetsLimit, poolId);
+    } else if (currentRound.endPrice == 0) {
+      console.log("Inside endprice==0 L119....");
+      currentRound.endPrice = price;
+
+      emit RoundEnded(poolId, timeMS, currentRound.startPrice, currentRound.endPrice, poolId);
+
+      distribute(poolId, batchSize, timeMS);
+    } else {
+      revert PendingDistributions();
+    }
+  }
+
+  function returnBets (
+    bytes calldata poolId,
+    BetGroup storage group,
+    uint32 batchSize
+  ) private {
+    console.log("Inside returnBets L135....");
+    uint256 pending = group.bets.length - group.distributedCount;
+    console.log("Pendings L137:",pending);
+    uint256 limit = pending > batchSize ? batchSize : pending;
+    console.log("Limit L139:",limit);
+    uint256 to = group.distributedCount + limit;
+    console.log("To L141:",to);
+
+    for (uint i = group.distributedCount; i < to; i ++) {
+      sendEther(group.addresses[i], group.bets[i]);
+      emit TradeReturned(poolId, group.addresses[i], group.bets[i], group.whiteLabelIds[i]);
+    }
+
+    group.distributedCount = to;
+  }
+
+  function distribute (
+    bytes calldata poolId,
+    uint32 batchSize,
+    int64 timeMS
+  ) public onlyGameController onlyPoolExists(poolId) {
+    Round storage round = pools[poolId];
+    console.log("Inside Distribute function L157......");
+
+        console.log("Inside Distribute UpBetsGroup length is L159:", round.upBetGroup.bets.length);
+        console.log("Inside Distribute downBetGroup length is L160", round.downBetGroup.bets.length ); 
+    if (round.upBetGroup.bets.length == 0 || round.downBetGroup.bets.length == 0 || (round.startPrice == round.endPrice)) 
+    {
+         
+      if (round.startPrice == round.endPrice){ //In case of TIE return the investments to both sides
+        console.log("If round is tie L165:");
+        BetGroup storage returnGroupUp = round.upBetGroup;
+        BetGroup storage returnGroupDown = round.downBetGroup;
+
+        uint fromReturnUp = returnGroupUp.distributedCount;
+         console.log("Up counted L170:",fromReturnUp);
+        uint fromReturnDown = returnGroupDown.distributedCount;
+        console.log("Down counted L172:",fromReturnDown);
+        returnBets(poolId, returnGroupUp, batchSize);
+        returnBets(poolId, returnGroupDown, batchSize);
+
+        emit RoundDistributed(poolId, returnGroupUp.bets.length, fromReturnUp, returnGroupUp.distributedCount,timeMS);
+        emit RoundDistributed(poolId, returnGroupDown.bets.length, fromReturnDown, returnGroupDown.distributedCount,timeMS);
+
+        if (returnGroupUp.distributedCount == returnGroupUp.bets.length && returnGroupDown.distributedCount == returnGroupDown.bets.length) {
+         console.log("Inside befor clearPool L180......");
+          clearPool(poolId);
+        }
+
+      }else{
+        BetGroup storage returnGroup = round.downBetGroup.bets.length == 0 ? round.upBetGroup : round.downBetGroup;
+         
+        uint fromReturn = returnGroup.distributedCount;
+        console.log("From return L188......", fromReturn);
+        returnBets(poolId, returnGroup, batchSize);
+        emit RoundDistributed(poolId, returnGroup.bets.length, fromReturn, returnGroup.distributedCount,timeMS);
+
+        if (returnGroup.distributedCount == returnGroup.bets.length) {
+          console.log("Inside befor clear pool L193......");
+          clearPool(poolId);
+        }
+      }
+ 
+      return;
+    }
+    console.log("Distribute L200......");
+    BetGroup storage winners = round.downBetGroup;
+    BetGroup storage losers = round.upBetGroup;
+
+    if (round.startPrice < round.endPrice) {
+         console.log("If start Price is leass than end price L198......");
+      winners = round.upBetGroup;
+      losers = round.downBetGroup;
+    }
+
+    Distribution memory dist = calculateDistribution(winners, losers);
+    uint256 limit = dist.pending > batchSize ? batchSize : dist.pending;
+    console.log("Limit L210:", limit);
+    uint256 to = winners.distributedCount + limit;
+    console.log("To L212:", to);
+
+    for (uint i = winners.distributedCount; i < to; i++) {
+      uint256 winnings = (winners.bets[i]  dist.totalFees  100 / winners.total  / 100);
+      console.log("Winnings L216:", winnings);
+      sendEther(winners.addresses[i], winnings + winners.bets[i]);
+      emit TradeWinningsSent(poolId, winners.addresses[i], winners.bets[i], winnings, winners.addresses[i], winners.whiteLabelIds[i],feePercentage,feeJackpotPercentage);
+      winners.totalDistributed = winners.totalDistributed + winnings;
+    }
+
+    emit RoundDistributed(poolId, winners.bets.length, winners.distributedCount, to, timeMS);
+
+    winners.distributedCount = to;
+    if (winners.distributedCount == winners.bets.length) {
+      console.log("inside if L226.......");
+      sendEther(feeAddress, (dist.fee + dist.totalMinusFee)*feePercentage / 100);
+      sendEther(feeJackpotAddress, (dist.feeJackpot + dist.totalMinusJackpotFee)*feeJackpotPercentage / 100);
+      //Send leftovers to fee address
+      sendEther(feeAddress,getContractBalance());
+
+      clearPool(poolId);
+    }
+  }
+
+   function calculateDistribution (
+    BetGroup storage winners,
+    BetGroup storage losers
+  ) private view returns (Distribution memory) {
+    console.log("Inside calculate distribution......");
+    uint256 fee = feePercentage * losers.total / 100;
+    console.log("Fee L237:", fee);
+    uint256 jackpotFee = feeJackpotPercentage * losers.total / 100;
+    console.log("jackpotFee L239:", jackpotFee);
+    uint256 totalFee = fee + jackpotFee;
+    uint256 pending = winners.bets.length - winners.distributedCount;
+    console.log("pending L242:", pending);
+    uint256 totalFees = losers.total - totalFee;
+    console.log("totalFees L244:", totalFees);
+    uint256 totalMinusFee = losers.total - fee;
+    console.log("totalMinusFee L246:", totalMinusFee);
+    uint256 totalMinusJackpotFee = losers.total - jackpotFee;
+
+    return Distribution({
+      fee: fee,
+      feeJackpot: jackpotFee,
+      totalMinusFee: totalMinusFee,
+      totalMinusJackpotFee: totalMinusJackpotFee,
+      totalFees: totalFees,
+      pending: pending
+    });
+  }
+
+  function clearPool (
+    bytes calldata poolId
+  ) private {
+    console.log("Inside clearPool L262.....");
+    delete pools[poolId].upBetGroup;
+    delete pools[poolId].downBetGroup;
+    delete pools[poolId].startPrice;
+    delete pools[poolId].endPrice;
+  }
+
+  function hasPendingDistributions(
+    bytes calldata poolId
+  ) public view returns (bool) {
+    return (pools[poolId].upBetGroup.bets.length + pools[poolId].downBetGroup.bets.length) > 0;
+  }
+
+
+  struct makeTradeStruct{
+    bytes poolId;
+    string avatarUrl;
+    string countryCode;
+    bool upOrDown;
+    string whiteLabelId;
+  }
+
+  struct userDataStruct{
+      string avatar;
+      string countryCode;
+      string whiteLabelId;
+      int64 roundStartTime;
+    }
+
+ function addBet (
+    BetGroup storage betGroup,
+    uint256 amount,
+    string calldata avatar,
+    string calldata countryCode,
+    string calldata whiteLabelId
+  ) private returns (uint256) {
+    console.log("Inside Add Bet......");
+    betGroup.bets.push(amount);
+    betGroup.addresses.push(msg.sender);
+    betGroup.avatars.push(avatar);
+    betGroup.countries.push(countryCode);
+    betGroup.whiteLabelIds.push(whiteLabelId);
+    betGroup.total += amount;
+    return betGroup.total;
+  }
+
+  function makeTrade(
+    makeTradeStruct calldata userTrade
+  ) public payable nonReentrant onlyOpenPool(userTrade.poolId) onlyGameRunning onlyPoolExists(userTrade.poolId) {
+    require(isEOA(msg.sender) || allowedContracts[msg.sender], "Caller must be EOA or allowed contract");
+    require(msg.value > 0, "Msg.value must be greater than zero");
+    require(msg.value >= pools[userTrade.poolId].minBetAmount, "Trade amount should be higher than the minimum");
+    require(msg.value <= pools[userTrade.poolId].maxBetAmount, "Trade amount should be lower than the maximum");
+   // console.log("Current block timestamp:", block.timestamp);
+    //Prevent making trade while end round transaction being confirmed on blockchain
+    require(block.timestamp <= pools[userTrade.poolId].tradesEndTimeMS , "Round is closing");
+
+    //Prevent making trade while round starts
+    require(block.timestamp >= pools[userTrade.poolId].tradesStartTimeMS , "Round not started yet");
+
+    uint256 newTotal;
+
+    if (userTrade.upOrDown) {
+      require(pools[userTrade.poolId].upBetGroup.bets.length <= pools[userTrade.poolId].poolBetsLimit-1,"Pool is full, wait for next round");
+      newTotal = addBet(pools[userTrade.poolId].upBetGroup, msg.value, userTrade.avatarUrl, userTrade.countryCode, userTrade.whiteLabelId);
+     console.log("Up trade users L326:", newTotal);
+    } else {
+      require(pools[userTrade.poolId].downBetGroup.bets.length <= pools[userTrade.poolId].poolBetsLimit-1,"Pool is full, wait for next round");
+      newTotal = addBet(pools[userTrade.poolId].downBetGroup, msg.value, userTrade.avatarUrl, userTrade.countryCode, userTrade.whiteLabelId);
+      console.log("Down trade users L330:", newTotal);
+    }
+
+    userDataStruct memory userTradeData;
+    userTradeData.avatar = userTrade.avatarUrl;
+    userTradeData.countryCode = userTrade.countryCode;
+    userTradeData.whiteLabelId = userTrade.whiteLabelId;
+    userTradeData.roundStartTime = pools[userTrade.poolId].roundStartTime;
+    
+    emit TradePlaced(userTrade.poolId, msg.sender, msg.value, (userTrade.upOrDown) ? "UP":"DOWN", newTotal, 
+    userTrade.poolId, msg.sender, userTradeData.avatar, userTradeData.countryCode, 
+    userTradeData.roundStartTime, userTradeData.whiteLabelId);
+  }
+
+//   function allowContract(address _contract) public onlyOwner {
+//       if (!allowedContracts[_contract]) {
+//           allowedContracts[_contract] = true;
+//           allowedContractsList.push(_contract);
+//       }
+//   }
+
+//   function removeContract(address _contract) public onlyOwner {
+//       if (allowedContracts[_contract]) {
+//           allowedContracts[_contract] = false;
+//           // Remove the contract from the allowedContractsList
+//           for (uint i = 0; i < allowedContractsList.length; i++) {
+//               if (allowedContractsList[i] == _contract) {
+//                   allowedContractsList[i] = allowedContractsList[allowedContractsList.length - 1];
+//                   allowedContractsList.pop();
+//                   break;
+//               }
+//           }
+//       }
+//   }
+
+//   function getAllowedContracts() public view returns (address[] memory) {
+//       return allowedContractsList;
+//   }
+
+  function getContractBalance() public view returns (uint256) {
+    return address(this).balance;
+  }
+
+  function isEOA(address _addr) private view returns (bool) {
+      // Checks if the caller is an EOA
+      return _addr == tx.origin;
+  }
+
+  function sendEther (
+    address to, 
+    uint256 amount
+  ) private {
+    (bool sent, bytes memory data) = payable(to).call{gas: 0, value: amount}("");
+    require(sent, "Couldn't send ether");
+  }
+  
+    // New receive function to handle incoming Ether transactions
+    receive() external payable {    }
+
+
+     // Methods
+  function changeGameControllerAddress(address newGameControllerAddress) public onlyOwner nonZeroAddress(newGameControllerAddress) {
+    // require(newGameControllerAddress != address(0x0) , "Address cannot be zero address");
+    gameController = newGameControllerAddress;
+  }
+
+  function changeGameFeePercentage(uint8 newFeePercentage) public onlyOwner {
+    require (newFeePercentage <=100 , "Wrong fee percentage value");
+
+    feePercentage = newFeePercentage;
+  }
+
+  function changeGameFeeJackpotPercentage(uint8 newFeeJackpotPercentage) public onlyOwner {
+    require (newFeeJackpotPercentage <=100 , "Wrong jackpot fee percentage value");
+
+    feeJackpotPercentage = newFeeJackpotPercentage;
+  }
+
+  function changeGameFeeAddress(address newFeeAddress) public onlyOwner nonZeroAddress(newFeeAddress){
+   // require(newFeeAddress != address(0x0) , "Address cannot be zero address");
+
+    feeAddress = newFeeAddress;
+  }
+
+  function changeGameFeeJackpotAddress(address newFeeJackpotAddress) public onlyOwner nonZeroAddress(newFeeJackpotAddress){
+    // require(newFeeJackpotAddress != address(0x0) , "Address cannot be zero address");
+
+    feeJackpotAddress = newFeeJackpotAddress;
+  }
+
+  function stopGame(bytes calldata reason) public onlyOwner {
+    isRunning = false;
+    notRunningReason = reason;
+    emit GameStopped(reason);
+  }
+
+  modifier onlyGameController () {
+    require(msg.sender == gameController, 'Only game controller can do this');
+    _;
+  }
+
+
+  modifier onlyGameRunning () {
+    require(isRunning, 'The game is not running');
+    _;
+  }
+
+  modifier onlyPoolExists (bytes calldata poolId) {
+    require(pools[poolId].created, 'Pool does not exist');
+    _;
+  }
+
+  modifier nonZeroAddress(address newAddress) {
+    require(newAddress != address(0), "Address cannot be zero address");
+    _;
+}
+
+}
+
